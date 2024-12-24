6853408a34f4be58a013ad75725c1813aaa3f493
BiserkaStikovic <147833012+BiserkaStikovic@users.noreply.github.com>
2024-12-24 22:28:04 +0100

    Add files via upload
    :)

---
+++ userServis/src/main/java/com/raf/listener/IncrementReservationCountListener.java
@@ -0,0 +1,31 @@
+package com.raf.listener;
+
+
+import com.raf.dto.IncrementReservationCountDto;
+import com.raf.service.impl.UserService;
+import com.raf.synCommunication.MessageHelper;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Component
+public class IncrementReservationCountListener {
+
+    private MessageHelper messageHelper;
+    private UserService userService;
+
+    public IncrementReservationCountListener(MessageHelper messageHelper, UserService userService) {
+        this.messageHelper = messageHelper;
+        this.userService = userService;
+    }
+
+    @JmsListener(destination = "${destination.incrementReservationCount}", concurrency = "5-10")
+    public void incrementReservationCount(Message message) throws JMSException {
+        IncrementReservationCountDto incrementReservationCountDto = messageHelper.getMessage(message, IncrementReservationCountDto.class);
+        System.out.println(incrementReservationCountDto);
+        userService.incrementReservationCount(incrementReservationCountDto);
+    }
+
+}
